0d566e4e9f32082cfb8820a765c4e7461fdb9143
liubei681 <ryancarpenterc257@gmail.com>
2024-08-09 15:04:40 +0800

    liubei

---
+++ lby_dev/build/CMakeFiles/stack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stack.dir/src/stack.obj"
+  "CMakeFiles/stack.dir/src/stack.obj.d"
+  "libstack.dll.a"
+  "stack.exe"
+  "stack.exe.manifest"
+  "stack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
