4a9a9a1026b0b53d0eeab1db9f7752d590d375c8
liubei681 <ryancarpenterc257@gmail.com>
2024-08-09 14:46:33 +0800

    xixi

---
+++ lby_dev/build/CMakeFiles/queue.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/queue.dir/src/queue.obj"
+  "CMakeFiles/queue.dir/src/queue.obj.d"
+  "libqueue.dll.a"
+  "queue.exe"
+  "queue.exe.manifest"
+  "queue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
